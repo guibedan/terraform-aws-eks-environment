9ccae34d0fb4e7bb9bcb30082b6630b4e65ea374
Guilherme Bedan <87638668+guibedan@users.noreply.github.com>
2026-02-05 01:33:19 -0300

    feat: add NAT gateway and private route tables

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -26,3 +26,11 @@ output "internet_gateway_id" {
 output "public_route_table_id" {
   value = aws_route_table.public.id
 }
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.this.id
+}
+
+output "private_route_table_id" {
+  value = aws_route_table.private.id
+}
